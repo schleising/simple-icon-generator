5a5351e7216d57b11e6d780eddab6bd8cb28b65d
schleising <stephen@schleising.net>
2025-06-19 12:09:55 +0100

    Refactor SVG to PNG conversion to generate multiple scaled versions

--- svg2png.py
+++ svg2png.py
@@ -5,9 +5,8 @@ import cairosvg
 logo_path = Path().home() / 'Downloads' / 'logos'
 
 for file in logo_path.iterdir():
-    # Open the .svg file
-    with open(file, 'r') as f:
-        svg = f.read()
-
     # Convert the .svg file to .png
-    cairosvg.svg2png(url=file.as_posix(), write_to=(logo_path / f'{file.stem}.png').as_posix())
+    cairosvg.svg2png(url=file.as_posix(), write_to=(logo_path / f'{file.stem}16.png').as_posix(), scale=1)
+    cairosvg.svg2png(url=file.as_posix(), write_to=(logo_path / f'{file.stem}32.png').as_posix(), scale=2)
+    cairosvg.svg2png(url=file.as_posix(), write_to=(logo_path / f'{file.stem}48.png').as_posix(), scale=3)
+    cairosvg.svg2png(url=file.as_posix(), write_to=(logo_path / f'{file.stem}128.png').as_posix(), scale=8)
